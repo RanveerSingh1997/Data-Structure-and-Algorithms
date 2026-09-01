f5fbb96f4129107976eae4d75579ba9a2f0be5d1
ranveer <namratc@zimetrics.com>
2026-09-01 22:51:14 +0530

    stack and reverse string question

--- src/stacks_queues/ReverseString.java
+++ src/stacks_queues/ReverseString.java
@@ -1,4 +1,21 @@
 package stacks_queues;
 
+import utils.StackTemplate;
+
 public class ReverseString {
+    public static String reverString(String str) {
+        StackTemplate<Character> stackList = new StackTemplate<>();
+        for (int i = 0; i < str.length(); i++) {
+            stackList.push(str.charAt(i));
+        }
+        StringBuilder reverseString = new StringBuilder();
+        while (!stackList.isEmpty()) {
+            reverseString.append(stackList.pop());
+        }
+        return reverseString.toString();
+    }
+
+    static void main(String[] args) {
+        System.out.println(reverString("ABCDER"));
+    }
 }
--- src/utils/StackTemplate.java
+++ src/utils/StackTemplate.java
@@ -3,31 +3,40 @@ package utils;
 import java.util.ArrayList;
 
 public class StackTemplate<T> {
-    private ArrayList<T> stackList = new ArrayList<>();
+    private final ArrayList<T> stackList = new ArrayList<>();
 
-    public  ArrayList<T> getStackList(){
-        return  stackList;
+    public ArrayList<T> getStackList() {
+        return stackList;
     }
 
-    public void printStack(){
-        for(int i=stackList.size()-1;i>=0;i++){
+    public void printStack() {
+        for (int i = stackList.size() - 1; i >= 0; i++) {
             System.out.println(stackList.get(i));
         }
     }
 
-    public boolean isEmpty(){
+    public int size() {
+        return stackList.size() - 1;
+    }
+
+    public boolean isEmpty() {
         return stackList.isEmpty();
     }
 
-    public  T peek(){
-        if(isEmpty()){
-            return  null;
-        }else {
-           return stackList.getLast();
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return stackList.getLast();
         }
     }
 
-    public void push(T value){
+    public void push(T value) {
         stackList.add(value);
+
+    }
+
+    public T pop() {
+        return stackList.removeLast();
     }
 }

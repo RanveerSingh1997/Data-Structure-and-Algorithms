ed8a18073ec56f1b67a7b5b6d7e5e7e0587261ae
ranveer <namratc@zimetrics.com>
2026-09-04 00:00:01 +0530

    Queue using stack

---
+++ src/utils/QueueWithStack.java
@@ -0,0 +1,46 @@
+package utils;
+
+
+public class QueueWithStack {
+    private final StackTemplate<Integer> stack1;
+    private final StackTemplate<Integer> stack2;
+
+
+    QueueWithStack() {
+        stack1 = new StackTemplate<>();
+        stack2 = new StackTemplate<>();
+    }
+
+    static void main() {
+        QueueWithStack queueWithStack = new QueueWithStack();
+        queueWithStack.enqueue(1);
+        queueWithStack.enqueue(2);
+        queueWithStack.enqueue(3);
+        queueWithStack.enqueue(4);
+        queueWithStack.enqueue(5);
+        queueWithStack.printQueue();
+
+    }
+
+    public void enqueue(int value) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(value);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public void printQueue() {
+        stack1.printStack();
+    }
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+}
